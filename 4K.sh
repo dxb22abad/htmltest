e3a34340b9fa9e1ce80dea75ef7fd95cbc371c45
root <root@ip-172-26-2-217.us-west-2.compute.internal>
2020-01-22 03:35:57 +0000

    new

--- 4K.sh
+++ 4K.sh
@@ -5,3 +5,8 @@ sed -i "s#\[LINK PROTOCOL\]##g"  /root/DiogenesList-master/htmltest/index_4K.htm
 sed -i "s#\[SOURCE ROOT\]##g"  /root/DiogenesList-master/htmltest/index_4K.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/DiogenesList-master/htmltest/index_4K.html
 
+
+cd /root/DiogenesList-master/htmltest/
+git add .
+git commit -m 'new'
+git push  https://github.com/dxb22abad/htmltest.git
