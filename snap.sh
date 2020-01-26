394933e8adc9a1553cf8da0d38dc16b642f98655
dxb22abad <dxb22abad@gmail.com>
2020-01-26 13:13:26 -0500

    test

--- snap.sh
+++ snap.sh
@@ -8,7 +8,7 @@ sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用_侵权联系https://t.me/dxb22abad(telegram)#g" /root/snap2html/DiogenesList-master/index.html
-sed -i "s#e>index#e>00盘_Nplayer播放体验更佳#g" /root/snap2html/DiogenesList-master/index.html
+sed -i "s#e>index#e>00盘_Nplayer适用#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
