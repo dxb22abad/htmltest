733495bdf8dba7cd6f233f3d31f56bc06b204820
dxb22abad <dxb22abad@gmail.com>
2020-01-27 20:34:00 -0500

    oracle

--- snap.sh
+++ snap.sh
@@ -8,7 +8,7 @@ sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#1>index#1>嫖自公开分享_个人提高用_侵权联系https://t.me/dxb22abad(telegram)#g" /root/snap2html/DiogenesList-master/index.html
-sed -i "s#e>index#e>00盘_Nplayer适用#g" /root/snap2html/DiogenesList-master/index.html
+sed -i "s#e>index#e>00盘_Nplayer适用By大学渣#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
