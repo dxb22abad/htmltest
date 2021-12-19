7d2b93213a29a21205f14160af464b5fb0669520
dxb22abad <dxb22abad@gmail.com>
2021-12-19 06:30:36 +0000

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -5,7 +5,7 @@ sed -i "s#\[LINK ROOT\]#https://dxzorz.dxz.plus#g"  /root/.conf.d/snap2html/Diog
 sed -i "s#\[LINK PROTOCOL\]##g"  /root/.conf.d/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /root/.conf.d/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00alpha##g"  /root/.conf.d/snap2html/DiogenesList-master/index.html
-sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用_侵权联系https://t.me/dxb22abad(telegram)#g" /root/snap2html/DiogenesList-master/index.html
+sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用_侵权联系https://t.me/dxb22abad(telegram)#g" /root/.conf.d/snap2html/DiogenesList-master/index.html
 sed -i "s#e>index#e>00盘_用法详见dxz.plus#g" /root/.conf.d/snap2html/DiogenesList-master/index.html
 cp /root/.conf.d/snap2html/DiogenesList-master/index.html /root/.conf.d/snapGit/
 cd /root/.conf.d/snapGit
